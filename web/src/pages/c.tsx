c60eaf020ea0afff2950bd9cc337090a14a4e40e
DewinP <dewinspena@gmail.com>
2020-12-14 14:12:43 -0500

    tried adding search bar

--- web/src/pages/c.tsx
+++ web/src/pages/c.tsx
@@ -1,5 +1,15 @@
-import { Box, Heading, Text, Stack } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Heading,
+  Stack,
+  Flex,
+  InputGroup,
+  Input,
+  InputRightElement,
+  IconButton,
+} from "@chakra-ui/react";
+import { FiSearch } from "react-icons/fi";
+import React, { useState } from "react";
 import { Layout } from "../components/Layout";
 import { useAllCommunitiesQuery } from "../generated/graphql";
 import { CommunityInfoBlock } from "../components/CommunityInfoBlock";
@@ -9,13 +19,13 @@ interface communitiesProps {}
 
 const communities: React.FC<communitiesProps> = ({}) => {
   const { data, error, loading } = useAllCommunitiesQuery();
-  console.log(data);
+  let [searchTerm, setSeatchTerm] = useState("");
+  const handleChange = (e) => {
+    console.log(searchTerm);
+    setSeatchTerm(e.target.value);
+  };
   if (loading) {
-    return (
-      <Layout>
-        <div>loading...</div>
-      </Layout>
-    );
+    return <Layout>...loading</Layout>;
   }
   if (error) {
     return <div>{error.message}</div>;
@@ -24,15 +34,31 @@ const communities: React.FC<communitiesProps> = ({}) => {
   return (
     <Layout hideSidebar>
       <Box mx="auto" px="10px">
-        <Heading>All Communites</Heading>
+        <Flex
+          justify="space-between"
+          align="center"
+          flexDir={{ base: "column", lg: "row" }}
+        >
+          <Heading>All Communites</Heading>
+          <InputGroup size="sm" maxW="400px">
+            <Input
+              value={searchTerm}
+              onChange={handleChange}
+              placeholder="search for a community"
+            />
+            <InputRightElement>
+              <IconButton
+                size="sm"
+                aria-label="search communities"
+                icon={<FiSearch />}
+              />
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
         <Stack spacing={2} mt={4}>
-          {data.allCommunties.length > 0 ? (
-            data?.allCommunties.map((c) => {
-              return <CommunityInfoBlock key={c.id} community={c} />;
-            })
-          ) : (
-            <Text> No communities at the moment</Text>
-          )}
+          {data?.allCommunties.map((c) => {
+            return <CommunityInfoBlock key={c.id} community={c} />;
+          })}
         </Stack>
       </Box>
     </Layout>
